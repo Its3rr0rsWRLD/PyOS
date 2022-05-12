6b36d9cfc57463794c1b9e5551ff9c71f10e0c20
TheIsolatedDev <92172873+School-Exploits@users.noreply.github.com>
2022-05-12 15:18:01 -0500

    Delete PyOS directory

--- PyOS/Commands/CD/cd.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-root = open("curcom.txt", "r").read()
-curcd = root.split("cd ")[1]
-with open("cdlocal.txt", "w") as f:
-    f.write(curcd)
-os.system(sys.executable + " main.py")

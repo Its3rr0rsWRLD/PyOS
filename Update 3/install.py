27e95b2d9cc23973325a3e2391616183407ff9b7
TheIsolatedDev <92172873+School-Exploits@users.noreply.github.com>
2022-05-18 17:28:32 -0500

    Create install.py

---
+++ Update 3/install.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+print("\nMay have a few errors, but PyOS should work.\n")
+time.sleep(2)
+
+os.system("pip install ast")
+os.system("pip install Constant")
+os.system("pip install webbrowser")
+os.system("pip install requests")

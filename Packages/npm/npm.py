48570ffce42a97064fc33a8d2ed8692175695ac7
TheIsolatedDev <92172873+School-Exploits@users.noreply.github.com>
2022-05-19 20:15:05 -0500

    Update npm.py

--- Packages/npm/npm.py
+++ Packages/npm/npm.py
@@ -1,10 +1,22 @@
 import os
+import sys
 import webbrowser
 
-answer = input("What platform are you on? (Mac/Windows) >")
-if answer == "mac" or answer == "Mac":
-  webbrowser.open("https://nodejs.org/dist/v16.15.0/node-v16.15.0.pkg"
-elif answer == "windows" or answer == "Windows:
-  webbrowser.open("https://nodejs.org/dist/v16.15.0/node-v16.15.0-x86.msi"
-                  
-os.system(sys.executable + " main.py")
+question = input("\nDo you have Node.js installed? (Y/N)")
+if question == "y" or question == "Y":
+  with open("npm.txt", "r") as f:
+    for line in f:
+        line = line.strip()
+        os.system("npm install " + line)
+        print("Installed " + line)
+        os.system(sys.executable + " main.py")
+if question == "n" or question == "N":
+    answer = input("What platform are you on? (Mac/Windows) > ")
+    if answer == "mac" or answer == "Mac":
+        webbrowser.open("https://nodejs.org/dist/v16.15.0/node-v16.15.0.pkg")
+    elif answer == "windows" or answer == "Windows":
+        webbrowser.open("https://nodejs.org/dist/v16.15.0/node-v16.15.0-x86.msi")
+                    
+    print("\nPlease install Node.js, then run this again.")
+                    
+    os.system(sys.executable + " main.py")
